7e4cb0622c634133bd15f5cdbe9edab19b01beb7
dsolanki <dsolanki@umich.edu>
2020-05-26 18:34:47 -0400

    Timer working

--- Dareduino/main.c
+++ Dareduino/main.c
@@ -1,5 +1,5 @@
 /*
- * ArduinoPlusPlus.c
+ * Dareduino.c
  *
  * Created: 24-05-2020 16:44:50
  * Author : Deepen
@@ -15,60 +15,10 @@
 #include "gpio.h"
 #include "macros.h"
 
-//void TIMER1_COMPA_vect(void) __attribute__((signal));
-
-//void scheduler();
-/*
-void timerInit()
-{
-	//Enable CTC mode by setting WGM12 bit. Set pre-scaler to 256 by setting CS12 bit
-	TCCR1B |= (1<<CS12) | (1<<WGM12);
-	
-	//Start TCNT1 from 0
-	TCNT1 = 0;
-	
-	//It will overflow when it reaches this compare value
-	OCR1A = 30000;
-	
-	//Enable interrupt on match
-	TIMSK1 |= (1<<OCIE1A);
-	
-	//Enable interrupts
-	sei();
-	
-}
-*/
-
 int main(void)
 {
 	pinConfig(13,OUTPUT);
 	timerInit();
 	while(1);
-	
-
-	/*
-	while(121)
-	{
-		pinWrite(13,1);
-		_delay_ms(500);
-		pinWrite(13,0);
-		_delay_ms(500);
-	}
-	*/
 }
 
-/*
-void taskScheduler()
-{
-	int a = pinRead(13);
-	pinWrite(13,!a);
-	printf("%s\n","In scheduler");
-}
-*/
-
-/*
-void TIMER1_COMPA_vect()
-{
-	scheduler();
-}
-*/
